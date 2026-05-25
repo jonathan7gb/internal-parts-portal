38b7917cdae0c2bc40315d7cb62607080bac368f
PabloTzeliks <arq.pabloo@gmail.com>
2026-05-25 15:53:11 -0300

    chore: add JJWT + flyway-postgresql deps and configure application.yml

--- src/main/java/com/centroweg/senai/system_deployment_project_api/SystemDeploymentProjectApiApplication.java
+++ src/main/java/com/centroweg/senai/system_deployment_project_api/SystemDeploymentProjectApiApplication.java
@@ -2,8 +2,10 @@ package com.centroweg.senai.system_deployment_project_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SystemDeploymentProjectApiApplication {
 
 	public static void main(String[] args) {
